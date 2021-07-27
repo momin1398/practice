a376d888d64ff12e1ab71fcba45bd54847e4bc33
Bahar Basha <momin.b.bahar.basha@accenture.com>
2021-07-27 16:01:33 +0530

    h

--- main.go
+++ main.go
@@ -3,16 +3,10 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-	m := 10
-	if m%2 == 0 {
-		fmt.Println("even num")
-=======
 	var c int
 	fmt.Scan(&c)
 	if c%2 == 0 {
-		fmt.Println("even")
->>>>>>> test
+		fmt.Println("even number")
 	}
 
 }
